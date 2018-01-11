87d67a7ea51d9b099ec5a654f60a6b5f8212e897
underbleu <underbleu.dev@gmail.com>
2018-01-11 17:08:33 +0900

    REST API활용한 할일목록 추가/삭제 기능구현 (update는 CORS문제로 미해결)

--- index.js
+++ index.js
@@ -29,26 +29,37 @@ addButtonEl.addEventListener('click', async e => {
 });
 
 async function addTodo(title) {
-  todos.push({
-    id: count++,
-    title,
-    complete: false
-  });
+  res = await fetch(`${API_URL}/todos`, {
+    method: 'post',
+    body: JSON.stringify({title, complete:false}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  refreshTodoList();
 }
 
 async function removeTodo(todoId) {
-  todos = todos.filter(t => t.id !== todoId);
+  return fetch(`${API_URL}/todos/${todoId}`, {
+    method: 'delete'
+  });
 }
 
 async function updateTodo(todoId, complete) {
-  for (let todo of todos) {
-    if (todo.id === todoId) {
-      todo.complete = complete;
+  return fetch(`${API_URL}/todos/${todoId}`, {
+    method: 'patch',
+    body: JSON.stringify({complete}),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }
+  });
 }
 
 async function refreshTodoList() {
+  // REST API에서 할 일 목록 가져오기
+  const res = await fetch(`${API_URL}/todos`);
+  const todos = await res.json();
+
   // 현재 화면의 할 일 목록 삭제
   todoListEl.innerHTML = '';
 
